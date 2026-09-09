f657d43be760c5194ecd224276e05acfe8bb4d25
NickTorrez <nickelias075@gmail.com>
2026-09-09 10:28:15 -0600

    Borrador 8.6

--- Data/OceanDeskDbContext.cs
+++ Data/OceanDeskDbContext.cs
@@ -47,8 +47,7 @@ public partial class OceanDeskDbContext : DbContext
     public virtual DbSet<TableRestaurant> TableRestaurants { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost\\SQL2025DEV;Database=Ocean_Desk_DB;Trusted_Connection=True;TrustServerCertificate=True;");
+    => optionsBuilder.UseSqlServer("Server=localhost\\SQL2025DEV;Database=Ocean_Desk_DB;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
